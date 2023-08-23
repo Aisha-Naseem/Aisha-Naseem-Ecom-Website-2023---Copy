27ee25098e2f6e9493bcd5d0428fa134cc44d239
Aisha-Naseem <126055186+Aisha-Naseem@users.noreply.github.com>
2023-08-24 00:01:00 +0500

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');   
 const Pizza = require('./models/pizzaModel');
 const db = require('./db');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const pizzasRoute = require('./routes/pizzaRoutes');
 const userRoute = require('./routes/userRoute');
-
-app.use('/api/pizzas', pizzasRoute);
 
 app.use('/api/pizzas/', pizzasRoute);
 app.use('/api/users/', userRoute);
